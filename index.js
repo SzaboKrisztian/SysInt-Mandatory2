fa0aaa321c92e4c3599fd9d083cb3682566f9636
Chris <szabo.krisztian.87@gmail.com>
2021-11-15 17:47:49 +0100

    Further work

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const path = require("path");
 const fs = require("fs/promises");
 
-const supportedFormats = {
-    json: 1,
-    xml: 2,
-    csv: 3
-}
+const supportedFormats = ['json', 'xml', 'csv'];
 
 const data = {
     subscribers: [],
@@ -15,6 +11,9 @@ const data = {
 const FS_ROOT = path.resolve('.');
 const FS_SUBS = path.resolve(FS_ROOT, 'subscribers');
 const FS_TOPICS = path.resolve(FS_ROOT, 'topics');
+
+fs.mkdir(FS_SUBS).catch(() => {});
+fs.mkdir(FS_TOPICS).catch(() => {});
 
 function getTopics() {
     return fs.readdir(FS_TOPICS, { withFileTypes: true })
@@ -28,19 +27,27 @@ function getSubscribers() {
         .then(files => files.map(sub => sub.name))
         .then(files => files.map(sub => fs.readFile(path.resolve(FS_SUBS, sub), 'utf-8')))
         .then(filesData => Promise.allSettled(filesData))
-        .then(subs => subs.map(sub => sub.value.split('\n')))
-        .then(subs => subs.filter(sub => sub.length > 1))
-        .then(subs => subs.map(sub => ({
-            name: sub[0].split(',')[0],
-            host: sub[0].split(',')[1],
-            topics: sub.slice(1)
-                .map(entry => entry.split(','))
-                .map(topic => ({ name: topic[0], format: topic[1] }))
-        }))
-    );
+        .then(subs => subs
+            .filter(sub => sub.status === 'fulfilled')
+            .map(sub => sub.value.split('\n'))
+            .filter(sub => sub.length > 1) // At least header + 1 sub
+            .map(sub => {
+                const [name, host] = sub[0].split(',');
+                const topics = sub.slice(1);
+                return {
+                    name,
+                    host,
+                    topics: topics
+                        .map(entry => entry.split(','))
+                        .map(topic => ({ name: topic[0], format: topic[1] }))
+                }
+            }));
 }
 
 function subscribe(name, host, subscriptions) {
+    if (!subscriptions.some(sub => supportedFormats.includes(sub.format))) {
+        throw new Error("Unknown format");
+    }
     const entry = {
         name,
         host,
@@ -48,20 +55,29 @@ function subscribe(name, host, subscriptions) {
     };
     data.subscribers.push(entry);
     subscriptions.forEach(sub => {
-        data.topics[sub.topic] = {
-            name,
-            host,
-            format: sub.format
-        };
+        const old = data.topics[sub.topic] ?? [];
+        data.topics[sub.topic] = [
+            ...old,
+            {
+                name,
+                host,
+                format: sub.format
+            }
+        ];
     });
     const topic = path.resolve(FS_TOPICS, name);
     fs.mkdir(topic)
-        .then(console.log('created folder', name))
-        .catch(console.log('error creating folder', name));
+        .catch(() => {});
 }
 
 function post(topic, message) {
+}
 
+function notify(topic, message) {
+    if (!data.topics[topic]) {
+        return;
+    }
+    data.topics[topic].map()
 }
 
 getTopics().then(console.log);
